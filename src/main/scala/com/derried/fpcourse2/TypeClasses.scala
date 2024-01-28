e431c3e434a11e32e6f1a33c0c421afc09828fb0
Alexander Babin <alexander_babin@mckinsey.com>
2024-01-28 22:46:10 +0700

    finish type class lesson 2

---
+++ src/main/scala/com/derried/fpcourse2/TypeClasses.scala
@@ -0,0 +1,77 @@
+package com.derried.fpcourse2
+
+import java.util.Date
+
+object TypeClasses extends App {
+
+  // Option 1
+  /* Disadvantages
+    - Can be only implement for OUR CLASSES
+    - Can have only 1 implementation
+   */
+  case class User(name: String, age: Int, email: String) {
+    def toHtml: String = s"<div>Hello, ${name} (${age} yo) <mailto='${email}'></div>"
+  }
+
+  // Option 2
+  /* Disadvantages
+    - Can be only implement for OUR CLASSES
+    - Not type safe
+    - Need to modify code all time for changes
+   */
+
+  def htmlRenderPM(value: Any): String = {
+    value match
+      case User(name, age, email) => ""
+      case _ => ""
+  }
+
+  // Option 3
+  trait HTMLSerializer[T] {
+    def serialize(value: T): String
+  }
+
+  object UserSerializer extends HTMLSerializer[User] {
+    override def serialize(user: User): String = s"<div>Hello, ${user.name} (${user.age} yo) <mailto='${user.email}'></div>"
+  }
+
+  /* Advantages
+  - More then one implementation
+  - Type safe
+  - Can be done for other types
+   */
+  object PrivateUserSerializer extends HTMLSerializer[User] {
+    override def serialize(user: User): String = s"<div>Hello, ${user.name}</div>"
+  }
+  val user = User("John", 20, "john@gmail.com")
+
+  println(UserSerializer.serialize(user))
+  println(PrivateUserSerializer.serialize(user))
+  object DateSerializer extends HTMLSerializer[Date] {
+    override def serialize(date: Date): String = s"<div>today is: ${date}</div>"
+  }
+
+  /* Exercise
+    Equal
+   */
+  trait Equal[T] {
+    def isEqual(x: T, y: T): Boolean
+  }
+
+  object Equal {
+    def apply[T](a: T, b: T)(implicit equal: Equal[T]) = equal.isEqual(a,b)
+  }
+
+  object PrivateUserEqual extends Equal[User] {
+    override def isEqual(x: User, y: User): Boolean = x.name == y.name
+  }
+
+  implicit object AuthUserEqual extends Equal[User] {
+    override def isEqual(x: User, y: User): Boolean = x.name == y.name && x.email == y.email
+  }
+
+  val user1 = User("John", 20, "john@gmail.com")
+  val user2 = User("John", 20, "john@rockthejvm.com")
+
+  println(s"the user is equal: ${Equal(user1, user2)}")
+}
